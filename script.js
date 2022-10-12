1c74b2927b06e8e720047c60d742e35ce85bffb1
Gabi Gurgel <gabigurgeluninorte@gmail.com>
2022-10-12 12:52:57 -0400

    interface do meu portifolio

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrimenu(){
+    let BarraMenuAberto = document.getElementById ('barra-menu')
+
+    if(BarraMenuAberto.style.width == "0px"){
+        BarraMenuAberto.style.width = "180px";
+    }else{
+        BarraMenuAberto.style.width = "0px";
+    }
+       
+}
